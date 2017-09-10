c9a8ef863363ebc2c457fbce4ef24e1796d5a2c7
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-09-10 15:32:32 -0400

    update

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(document).mousemove(function(e){
     let middleY = $(document).height()/2;
     
     let X = e.pageX - middleX;
-    let Y = e.pageY - middleY;
+    let Y = e.pageY - middleY * 2;
 
     var layers = document.getElementsByClassName("parallax");
     var layer, speed, yPos, xPos;
@@ -11,7 +11,7 @@ $(document).mousemove(function(e){
         layer = layers[i];
         speed = layer.getAttribute('data-speed');
         var yPos = -(Y * speed / 250);
-        var xPos = -(X * speed / 500);
+        var xPos = -(X * speed / 250);
         layer.setAttribute('style', `transform: translate3d(${xPos}px, ${yPos}px, 0px)`);
     }
 }); 
