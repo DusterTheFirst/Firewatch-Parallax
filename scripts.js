1447cf063790d88c489a360f22f9fcce6815b1fb
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-09-09 21:18:03 -0400

    seperated the files

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).mousemove(function(e){ 
+    let middleX = $(document).width()/2;
+    let middleY = $(document).height()/2;
+    
+    let X = e.pageX/* - middleX;*/
+    let Y = e.pageY/* - middleY;*/
+    console.log(`${e.pageX}:${middleX}:${X}, ${e.pageY}:${middleY}:${Y}`); 
+
+    var layers = document.getElementsByClassName("parallax");
+    var layer, speed, yPos, xPos;
+    for (var i = 0; i < layers.length; i++) {
+        layer = layers[i];
+        speed = layer.getAttribute('data-speed');
+        var yPos = -(Y * speed / 250);
+        var xPos = -(X * speed / 500);
+        layer.setAttribute('style', `transform: translate3d(${xPos}px, ${yPos}px, 0px)`);
+
+    }
+}); 
